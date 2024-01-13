3a6330bd979e470e089d40470788eee41439b589
Steffen <theevilreaper@onelitefeather.net>
2024-01-13 21:50:32 +0000

    Update dependencies and the handling of them

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    id("org.springframework.boot") version "3.1.5"
-    id("io.spring.dependency-management") version "1.1.3"
-    kotlin("jvm") version "1.9.20"
-    kotlin("plugin.spring") version "1.9.20"
+    alias(libs.plugins.spring)
+    alias(libs.plugins.spring.dependency)
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.spring)
 }
 
 group = "net.theevilreaper"
@@ -47,16 +47,14 @@ repositories {
 }
 
 dependencies {
-    implementation("net.theevilreaper.vulpes.api:vulpes-spring-api:0.0.1+1f9ff402")
-    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.+")
+    implementation(libs.vulpes.api)
+    implementation(libs.spring.starter.web)
+    implementation(libs.spring.starter.data.mongodb)
+    implementation(libs.jackson)
 
-    testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo.spring30x:4.6.2")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    testImplementation(libs.embed.mongo)
+    testImplementation(libs.spring.starter.test)
+    testImplementation(libs.spring.starter.data.mongodb)
 }
 
 tasks {
