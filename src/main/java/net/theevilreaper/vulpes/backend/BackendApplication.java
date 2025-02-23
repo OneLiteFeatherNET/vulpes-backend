8916f7d19809d8a5a644dab7dc3f0afb28fec34b
Joltras <MinnyMouseXD@gmail.com>
2025-02-23 17:42:43 +0100

    Add BackendApplication

---
+++ src/main/java/net/theevilreaper/vulpes/backend/BackendApplication.java
@@ -0,0 +1,10 @@
+package net.theevilreaper.vulpes.backend;
+
+import io.micronaut.runtime.Micronaut;
+
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        Micronaut.run(BackendApplication.class, args);
+    }
+}
