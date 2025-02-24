2ffc06ef3b518b089a9fc3e6f971e1fa0cd44be9
theEvilReaper <steffenwx@gmail.com>
2025-02-24 09:32:09 +0100

    Migrate tests

---
+++ src/test/java/net/theevilreaper/vulpes/backend/exception/ErrorResponseTest.java
@@ -0,0 +1,33 @@
+package net.theevilreaper.vulpes.backend.exception;
+
+import io.micronaut.http.HttpMethod;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Test specific cases from the ErrorResponse class")
+class ErrorResponseTest {
+
+    static Stream<Arguments> invalidResponseCreation() {
+        return Stream.of(
+                Arguments.of(HttpMethod.GET, -1, "Test", "The status code is not in the valid range"),
+                Arguments.of(HttpMethod.OPTIONS, 1000, "Test", "The status code is not in the valid range")
+        );
+    }
+
+    @ParameterizedTest(name = "Test creation for status {0} and message {1}")
+    @MethodSource("invalidResponseCreation")
+    void testInvalidErrorResponseCreation(HttpMethod httpMethod, int statusCode, String message, String expectedMessage) {
+        String httpMethodName = httpMethod.name();
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> new ErrorResponse(httpMethodName, statusCode, message)
+        );
+        assertEquals(expectedMessage, exception.getMessage());
+    }
+}
