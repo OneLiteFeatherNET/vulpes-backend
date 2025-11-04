43271be0726bc3f243ff3048757887e81dc9deca
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-11-04 14:33:02 +0100

    chore(attribute): add improved value validation

--- src/main/java/net/onelitefeather/vulpes/backend/domain/attribute/AttributeModelDTO.java
+++ src/main/java/net/onelitefeather/vulpes/backend/domain/attribute/AttributeModelDTO.java
@@ -3,6 +3,10 @@ package net.onelitefeather.vulpes.backend.domain.attribute;
 import io.micronaut.core.annotation.Introspected;
 import io.micronaut.serde.annotation.Serdeable;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import net.onelitefeather.vulpes.api.model.AttributeEntity;
 
 import java.util.UUID;
@@ -13,9 +17,9 @@ import java.util.UUID;
 public record AttributeModelDTO(
         @Schema(description = "ID of the attribute", requiredMode = Schema.RequiredMode.NOT_REQUIRED) UUID id,
         @Schema(description = "The name for the ui", requiredMode = Schema.RequiredMode.REQUIRED) String uiName,
-        @Schema(description = "The name which represents the variable after the generation", requiredMode = Schema.RequiredMode.REQUIRED) String variableName,
-        @Schema(description = "Default value of the attribute", requiredMode = Schema.RequiredMode.REQUIRED) double defaultValue,
-        @Schema(description = "Maximum value of the attribute", requiredMode = Schema.RequiredMode.REQUIRED) double maximumValue
+        @Schema(description = "The name which represents the variable after the generation", requiredMode = Schema.RequiredMode.REQUIRED) @NotEmpty @NotBlank String variableName,
+        @Schema(description = "Default value of the attribute", requiredMode = Schema.RequiredMode.REQUIRED) @PositiveOrZero double defaultValue,
+        @Schema(description = "Maximum value of the attribute", requiredMode = Schema.RequiredMode.REQUIRED) @Positive double maximumValue
 ) {
     public AttributeEntity toAttributeModel() {
         return new AttributeEntity(id, uiName, variableName, defaultValue, maximumValue);
