aec1108693b7cc353e94d66261c2bc0878356ef8
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-11-13 20:02:37 +0100

    chore(imports): cleanup unused imports

--- src/main/java/net/onelitefeather/vulpes/backend/domain/notification/NotificationModelDTO.java
+++ src/main/java/net/onelitefeather/vulpes/backend/domain/notification/NotificationModelDTO.java
@@ -6,7 +6,6 @@ import io.micronaut.core.annotation.Introspected;
 import io.micronaut.serde.annotation.Serdeable;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import net.onelitefeather.vulpes.api.model.NotificationEntity;
 
